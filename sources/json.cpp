568c9925aa846065175277cb0f90c3ebb6010165
rasulbabaev <32385063+rasulbabaev@users.noreply.github.com>
2019-01-16 21:20:14 +0300

    Update json.cpp

--- sources/json.cpp
+++ sources/json.cpp
@@ -1,41 +1,184 @@
-// Copyright 2018 Alex Chubukov alexchubukov12345@gmail.com
+#include "stdafx.h"
 
-//#include "../third-party/json.hpp"
-#include <iostream>
-#include <nlohmann/json.hpp>
+#include "Json.hpp"
+#include "Commons.hpp"
 
-using json = nlohmann::json;
+using namespace std;
 
-int main(){
-    json empty_array = json::array();
-    json my_json = json::array({
-        {"Si-9.15", "RTS-9.15", "GAZP-9.15"},
-        {100024, 100027, 100050},
-        {"Futures contract for USD/RUB", "Futures contract for index RTS",
-        "Futures contract for Gazprom shares"}
-    });
 
-    std::size_t count = 0;
-    for (std::size_t j = 0; j < my_json.begin()->size(); ++j) {
-        count = 0;
-        json temp = json::object();
-        for (std::size_t i = 0; i <my_json.size(); ++i, ++count) {
-            switch (count) {
-            case 0:
-                temp["ticker"] = my_json[i][j];
-                break;
-            case 1:
-                temp["id"] = my_json[i][j];
-                break;
-            case 2:
-                temp["description"] = my_json[i][j];
-                break;
-            }
-        }
-        empty_array.push_back(temp);
-    }
-    my_json = empty_array;
-    std::cout << my_json << std::endl;
+Json::Json(const string & s)
+{
+	int i = 0;
+	i = MissSpaces(s, i);
 
-    return 0;
+	if (s[i] == '{')
+	{
+		_isArray = false;
+		_obj = GetMap(s, i + 1);
+	}
+	else if (s[i] == '[')
+	{
+		_isArray = true;
+		_arr = GetArray(s, i + 1);
+	}
+	else
+		throw exception("String is not valid!");
+}
+
+bool Json::is_array() const
+{
+	return _isArray;
+}
+
+bool Json::is_object() const
+{
+	return !_isArray;
+}
+
+int Json::GetArrSize() const
+{
+	if (is_array())
+		return _arr.size();
+	else
+		throw;
+}
+
+pair<any, int> GetValueAndLen(const string &s, int start)
+{
+	any result;
+	int i = start;
+	int endObj = 0;
+
+	if (s[i] == '{')
+	{
+		endObj = FindEnd(s, i, '{', '}');
+
+		string str = s.substr(i, endObj - i);
+		result = Json(str);
+		i = endObj;
+	}
+	else if (s[i] == '[')
+	{
+		endObj = FindEnd(s, i, '[', ']');
+
+		string str = s.substr(i, endObj - i);
+		result = Json(str);
+		i = endObj;
+	}
+	else if (s[i] == '\"')
+	{
+		string str = GetString(s, i);
+		i += str.length() + 2;
+		result = str;
+	}
+	else if (IsNum(s[i]))
+	{
+		pair<any, int> p = GetNumWithLen(s, i);
+		i += p.second;
+		result = p.first;
+	}
+	else if (s.substr(i, 5) == "false")
+	{
+		i += 5;
+		result = false;
+	}
+	else if (s.substr(i, 4) == "true")
+	{
+		i += 4;
+		result = true;
+	}
+	else if (s.substr(i, 4) == "null")
+	{
+		i += 4;
+		result = nullptr;
+	}
+	else
+		throw exception("String is not Valid!");
+
+	return pair(result, i - start);
+}
+
+std::any & Json::operator[](const std::string & key)
+{
+	if (is_object())
+		return _obj[key];
+
+	throw exception("It is not a object!");
+}
+
+std::any & Json::operator[](int index)
+{
+	if (is_array())
+		return _arr[index];
+
+	throw exception("This is not array!");
+}
+
+Json Json::parse(const std::string & s)
+{
+	return Json(s);
+}
+
+vector<any> Json::GetArray(const string &s, int start) const
+{
+	vector<any> result;
+	int i = start;
+
+	while (i < s.length())
+	{
+		i = MissSpaces(s, i);
+		pair<any, int> p = GetValueAndLen(s, i);
+
+		i += p.second;
+		result.push_back(p.first);
+
+		i = MissSpaces(s, i);
+		if (s[i] == ']')
+			break;
+
+		if (s[i++] == ',')
+			continue;
+
+		throw exception("String is not Valid");
+	}
+
+	return result;
+}
+
+std::map<std::string, std::any> Json::GetMap(const std::string & s, int start) const
+{
+	std::map<std::string, std::any> result;
+
+	int i = start;
+
+	while (i < s.length())
+	{
+		i = MissSpaces(s, i);
+		if (s[i] != '\"')
+			throw exception("String is not valid!");
+
+		string key = GetString(s, i);
+		i += key.length() + 2;
+		i = MissSpaces(s, i);
+		if (s[i++] != ':')
+			throw exception("String is not valid!");
+
+		i = MissSpaces(s, i);
+		pair<any, int> p = GetValueAndLen(s, i);
+
+		result[key] = p.first;
+
+		i += p.second;
+
+		i = MissSpaces(s, i);
+		if (s[i] == '}')
+			break;
+
+		if (s[i++] == ',')
+			continue;
+
+		throw exception("String is not Valid");
+	}
+
+	return result;
 }
